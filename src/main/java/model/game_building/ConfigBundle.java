2b2e832327147d600a39cc7119f117b2c579ee85
waleedbaroudi <waleedbaroudi@hotmail.com>
2020-11-18 19:05:19 +0300

    add getters to config bundle

--- src/main/java/model/game_building/ConfigBundle.java
+++ src/main/java/model/game_building/ConfigBundle.java
@@ -1,10 +1,10 @@
 package model.game_building;
 
 public class ConfigBundle {
-    int numOfAtomsPerType, numOfBlockersPerType, numOfPowerUpsPerType, numOfMoleculePerType;
-    double l;
-    boolean isLinearAlpha,isLinearBeta , isSpinningAlpha, isSpinningBeta ; //TODO: convert to enums
-    int difficulty; //0, 1, 2 for easy, normal, difficult, respectively. TODO: convert to enum
+    private int numOfAtomsPerType, numOfBlockersPerType, numOfPowerUpsPerType, numOfMoleculePerType;
+    private double l;
+    private boolean isLinearAlpha,isLinearBeta , isSpinningAlpha, isSpinningBeta ; //TODO: convert to enums
+    private int difficulty; //0, 1, 2 for easy, normal, difficult, respectively. TODO: convert to enum
 
     public ConfigBundle(int numOfAtomsPerType, int numOfBlockersPerType, int numOfPowerUpsPerType, int numOfMoleculePerType, double l, boolean isLinearAlpha, boolean isLinearBeta, boolean isSpinningAlpha, boolean isSpinningBeta, int difficulty) {
         this.numOfAtomsPerType = numOfAtomsPerType;
@@ -17,5 +17,45 @@ public class ConfigBundle {
         this.isSpinningAlpha = isSpinningAlpha;
         this.isSpinningBeta = isSpinningBeta;
         this.difficulty = difficulty;
+    }
+
+    public int getNumOfAtomsPerType() {
+        return numOfAtomsPerType;
+    }
+
+    public int getNumOfBlockersPerType() {
+        return numOfBlockersPerType;
+    }
+
+    public int getNumOfPowerUpsPerType() {
+        return numOfPowerUpsPerType;
+    }
+
+    public int getNumOfMoleculePerType() {
+        return numOfMoleculePerType;
+    }
+
+    public double getL() {
+        return l;
+    }
+
+    public boolean isLinearAlpha() {
+        return isLinearAlpha;
+    }
+
+    public boolean isLinearBeta() {
+        return isLinearBeta;
+    }
+
+    public boolean isSpinningAlpha() {
+        return isSpinningAlpha;
+    }
+
+    public boolean isSpinningBeta() {
+        return isSpinningBeta;
+    }
+
+    public int getDifficulty() {
+        return difficulty;
     }
 }
