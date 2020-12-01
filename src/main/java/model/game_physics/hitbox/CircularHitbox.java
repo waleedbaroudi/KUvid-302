ed91c9b23a4ab14fa8fb345d800aaa55c3e4c06a
Moayed Haji Ali <mali18@ku.edu.tr>
2020-12-01 04:26:13 +0300

    refine hitbox abstract class

--- src/main/java/model/game_physics/hitbox/CircularHitbox.java
+++ src/main/java/model/game_physics/hitbox/CircularHitbox.java
@@ -4,62 +4,41 @@ import utils.Coordinates;
 import utils.MathUtils;
 import utils.Vector;
 
+import java.util.ArrayList;
+
 public class CircularHitbox extends Hitbox {
-
-    private Vector arcVector;
-
+    
     private double angle;
+    private double radius;
     private final int NUMBER_OF_POINTS = 8;
 
-    public CircularHitbox(Vector arcVector){
-        Coordinates positionCoordinates = new Coordinates(arcVector.getOriginCoordinate().getX() +
-                arcVector.getPositionCoordinate().getX(), arcVector.getOriginCoordinate().getY() +
-                arcVector.getPositionCoordinate().getY());
-        this.arcVector = new Vector(arcVector.getOriginCoordinate(), positionCoordinates);
+    public CircularHitbox(double radius){
+        this.radius = radius;
         this.angle = 0;
     }
-
-    public void setArcVector(Vector arcVector) {
-        this.arcVector = arcVector;
-    }
-
-    public Vector getArcVector() {
-        return this.arcVector;
-    }
-
+    
     /**
      * rotates the hitbox of the owner object
      */
     @Override
     public void rotate(double angle) {
-        Coordinates newPositionCoordinates = MathUtils.applyRotation(angle, arcVector.getOriginCoordinate(), arcVector.getPositionCoordinate());
-        arcVector.setPositionCoordinate(newPositionCoordinates);
         this.angle += angle;
     }
 
     @Override
-    public boolean isInside(Coordinates ownerCoordinates, Coordinates objectCoordinates) {
-        objectCoordinates = MathUtils.applyRotation(-this.angle, ownerCoordinates, objectCoordinates);
-        Coordinates translationAmount = MathUtils.translationAmount(ownerCoordinates, this.arcVector.getOriginCoordinate());
-        objectCoordinates = MathUtils.translate(objectCoordinates, translationAmount);
-        return MathUtils.isWithinCircle(MathUtils.vectorMagnitude(this.arcVector), this.arcVector.getOriginCoordinate(), objectCoordinates);
+    public boolean isInside(Coordinates entityCoords, Coordinates checkCoords) {
+        checkCoords = MathUtils.applyRotation(-this.angle, entityCoords, checkCoords);
+        return MathUtils.isWithinCircle(entityCoords, this.radius, checkCoords);
     }
 
     @Override
-    public boolean isInside(Coordinates ownerCoordinates, Coordinates[] coordinates) {
-        for (Coordinates c : coordinates){
-            if (this.isInside(ownerCoordinates, c))
-                return true;
-        }
-        return false;
+    public ArrayList<Coordinates> getBoarderPoints(Coordinates entityCoords){
+        Vector arcVector = new Vector(entityCoords, new Coordinates(entityCoords.getX()+this.radius, entityCoords.getY()));
+        return MathUtils.coordinatesAroundCircle(arcVector, NUMBER_OF_POINTS);
     }
 
-    @Override
-    public Coordinates[] getBoundaryCoordinates(){
-    return MathUtils.coordinatesAroundCircle(arcVector, NUMBER_OF_POINTS);
-    }
     @Override
     public String toString() {
-        return "CircularHitbox: radius = " + MathUtils.vectorMagnitude(this.arcVector) +", angle = " + angle;
+        return "CircularHitbox: radius = " + this.radius +", angle = " + angle;
     }
 }
--- src/main/java/model/game_physics/hitbox/Hitbox.java
+++ src/main/java/model/game_physics/hitbox/Hitbox.java
@@ -4,6 +4,8 @@ import model.game_entities.enums.Direction;
 import utils.Coordinates;
 import utils.Vector;
 
+import java.util.ArrayList;
+
 public abstract class Hitbox {
 
     /**
@@ -14,18 +16,28 @@ public abstract class Hitbox {
 
     /**
      * checks if coordinates is inside the Hitbox
-     * @param coordinates
+     * @param entityCoords
+     * @param checkCoords
      * @return true if coordinates is inside the Hitbo, otherwise returns false
      */
-    public abstract boolean isInside(Coordinates ownerCoordinates, Coordinates coordinates);
+    public abstract boolean isInside(Coordinates entityCoords, Coordinates checkCoords);
+
+    public abstract ArrayList<Coordinates> getBoarderPoints(Coordinates entityCoords);
 
     /**
      * Checks if a Hitbox is collided with a another Hitbox.
-     * @param ownerCoordinates The owner object coordinates.
-     * @param coordinates the target list of coordinates.
+     * @param entityCoords The owner object coordinates.
+     * @param checkList the target list of coordinates.
      * @return True if the owner Hitbox is collided with the target Hitbox.
      */
-    public abstract boolean isInside(Coordinates ownerCoordinates, Coordinates[] coordinates);
+    public boolean isInside(Coordinates entityCoords, ArrayList<Coordinates> checkList){
+        for(Coordinates checkPoint : checkList){
+            if(!this.isInside(entityCoords, checkPoint)){
+                return false;
+            }
+        }
+        return true;
+    }
 
     public abstract Coordinates[] getBoundaryCoordinates();
 }
