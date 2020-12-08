57799f5b0a4317fbc689ffcf939ea5f28a436481
Abdul <manofsteel272@yahoo.com>
2020-12-08 22:20:03 +0300

    Modified Blender

--- src/main/java/model/game_running/Blender.java
+++ src/main/java/model/game_running/Blender.java
@@ -1,12 +1,30 @@
 package model.game_running;
 
+import model.game_entities.Atom;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Blender {
 
+    private ArrayList<Atom> container;
+
+    public Blender(ArrayList<Atom> container){
+        this.container = container;
+    }
 
 
+    public void blendAtom(Atom blendAtom, Atom resultAtom){
 
+    }
 
+    public void breakAtom(Atom blendAtom, Atom resultAtom){
 
+    }
 
-
+    private void addAtomsToContainer(Atom atom){
+        this.container.add(atom);
+    }
+    private void addAtomsToContainer(Atom[] atom){
+        this.container.addAll(Arrays.asList(atom));
+    }
 }
