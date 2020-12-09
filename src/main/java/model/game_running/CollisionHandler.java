57fffe6fada4be7bda272e47fe40d265d21e1cf9
Mouafak alsaid hasan <mhasan18@ku.edu.tr>
2020-12-10 01:22:55 +0300

    some adjustment on collisionHandler

--- src/main/java/model/game_running/CollisionHandler.java
+++ src/main/java/model/game_running/CollisionHandler.java
@@ -2,36 +2,33 @@ package model.game_running;
 
 import model.game_entities.*;
 
-public class CollisionHandler implements CollisionVisitor{
+public class CollisionHandler implements CollisionVisitor {
 
     RunningMode controller;
-    CollisionHandler(RunningMode controller){
+
+    CollisionHandler(RunningMode controller) {
         this.controller = controller;
     }
 
-    private void defaultCollision(AutonomousEntity entity1, AutonomousEntity entity2){
+    private void defaultCollision(AutonomousEntity entity1, AutonomousEntity entity2) {
         controller.removeEntity(entity1);
         controller.removeEntity(entity2);
     }
 
+    private int incrementScore(int score){ return score++;}
 
 
     @Override
     public void handleCollision(Atom atom, Molecule molecule) {
         defaultCollision(atom, molecule);
+        int score = 0;
+        incrementScore(score);
     }
 
     @Override
     public void handleCollision(Atom atom, Blocker blocker) {
         defaultCollision(atom, blocker);
     }
-
-    @Override
-    public void handleCollision(Atom atom, Shooter shooter) {
-        controller.removeEntity(atom);
-    }
-
-
 
     @Override
     public void handleCollision(Powerup powerup, Blocker blocker) {
@@ -44,56 +41,14 @@ public class CollisionHandler implements CollisionVisitor{
     }
 
 
-
     @Override
     public void handleCollision(Molecule molecule, Blocker blocker) {
     }
 
-    @Override
-    public void handleCollision(Molecule molecule, Shooter shooter) {
-    }
-
-    @Override
-    public void handleCollision(Molecule molecule, Atom atom2) {
-    }
-
-    @Override
-    public void handleCollision(Blocker blocker, Powerup powerup) {
-    }
-
-    @Override
-    public void handleCollision(Blocker blocker, Molecule molecule) {
-    }
-
-    @Override
-    public void handleCollision(Blocker blocker1, Blocker blocker2) {
-    }
 
     @Override
     public void handleCollision(Blocker blocker, Shooter shooter) {
     }
 
-    @Override
-    public void handleCollision(Blocker blocker, Atom atom2) {
-    }
 
-    @Override
-    public void handleCollision(Shooter shooter, Powerup powerup) {
-    }
-
-    @Override
-    public void handleCollision(Shooter shooter, Molecule molecule) {
-    }
-
-    @Override
-    public void handleCollision(Shooter shooter, Blocker blocker) {
-    }
-
-    @Override
-    public void handleCollision(Shooter shooter1, Shooter shooter2) {
-    }
-
-    @Override
-    public void handleCollision(Shooter shooter, Atom atom2) {
-    }
 }
--- src/main/java/model/game_running/CollisionVisitor.java
+++ src/main/java/model/game_running/CollisionVisitor.java
@@ -23,21 +23,21 @@ public interface CollisionVisitor {
     default void handleCollision(Molecule molecule1, Molecule molecule2){};
     void handleCollision(Molecule molecule, Blocker blocker);
     default void handleCollision(Molecule molecule, Shooter shooter){};
-    void handleCollision(Molecule molecule, Atom atom2);
+    default void handleCollision(Molecule molecule, Atom atom2){};
 
 
     // Blocker collision
-    void handleCollision(Blocker blocker, Powerup powerup);
-    void handleCollision(Blocker blocker, Molecule molecule);
+    default void handleCollision(Blocker blocker, Powerup powerup){};
+    default void handleCollision(Blocker blocker, Molecule molecule){};
     default void handleCollision(Blocker blocker1, Blocker blocker2){};
     void handleCollision(Blocker blocker, Shooter shooter);
-    void handleCollision(Blocker blocker, Atom atom2);
+    default void handleCollision(Blocker blocker, Atom atom2);
 
 
     // Shooter collision
-    void handleCollision(Shooter shooter, Powerup powerup);
+    default void handleCollision(Shooter shooter, Powerup powerup){};
     default void handleCollision(Shooter shooter, Molecule molecule){};
-    void handleCollision(Shooter shooter, Blocker blocker);
+    default void handleCollision(Shooter shooter, Blocker blocker){};
     default void handleCollision(Shooter shooter1, Shooter shooter2){};
     default void handleCollision(Shooter shooter, Atom atom2){};
 
