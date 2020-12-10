3d999aa9b6d2353b466ceb322031e44e8f4eda63
waleedbaroudi <waleedbaroudi@hotmail.com>
2020-12-10 14:30:28 +0300

    fix blender types mismatching

--- src/main/java/model/game_entities/Shooter.java
+++ src/main/java/model/game_entities/Shooter.java
@@ -73,7 +73,8 @@ public class Shooter extends Entity {
 
     public Atom nextAtom() {
         // TODO: change the atom types to random
-        return container.getAtom(this.getCoordinates(), EntityType.BETA);
+//        return container.getAtom(this.getCoordinates(), EntityType.BETA);
+        return null; //TODO: CHANGE THIS
 //        return new Atom(this.getCoordinates(), HitboxFactory.getInstance().getAtomHitbox(), PathPatternFactory.getInstance().getAtomPathPattern(), EntityType.BETA);
     }
 
--- src/main/java/model/game_space/Blender.java
+++ src/main/java/model/game_space/Blender.java
@@ -6,39 +6,41 @@ import utils.MathUtils;
 
 
 public class Blender {
-    private final double[][] BLENDING_MATRIX = {{1,2,3,4}, {.5, 1, 1 ,1.5}, {0.333, 0.333, 1, 0.666}, {.25,.25,.25,1}}; // TODO: Change to something cleaner.
+    private final double[][] BLENDING_MATRIX = {{1, 2, 3, 4}, {.5, 1, 1, 1.5}, {0.333, 0.333, 1, 0.666}, {.25, .25, .25, 1}}; // TODO: Change to something cleaner.
     private ProjectileContainer projectileBlender;
 
     private BlenderListener blenderListener;
 
-    public Blender(BlenderListener blenderListener, ProjectileContainer projectileBlender){
+    public Blender(BlenderListener blenderListener, ProjectileContainer projectileBlender) {
         // get the container through a static method since the runningMode will be a singleton
         // this.container = container;
         this.projectileBlender = projectileBlender;
         this.blenderListener = blenderListener;
-   }
+    }
 
-    public void blend(int sourceAtom, int destinationAtom){
-        System.out.println(projectileBlender.decreaseAtoms(sourceAtom,(int) Math.ceil(sourceAtom * BLENDING_MATRIX[sourceAtom - 1][destinationAtom - 1])));
-        System.out.println(projectileBlender.increaseAtoms(destinationAtom, (int) Math.ceil(sourceAtom * BLENDING_MATRIX[destinationAtom - 1][sourceAtom - 1])));
+    public void blend(int sourceAtom, int destinationAtom) {
+        System.out.println(projectileBlender.decreaseAtoms(sourceAtom, (int) Math.ceil(sourceAtom * BLENDING_MATRIX[sourceAtom - 1][destinationAtom - 1])));
+        System.out.println(projectileBlender.increaseAtoms(destinationAtom, (int) Math.ceil(destinationAtom * BLENDING_MATRIX[destinationAtom - 1][sourceAtom - 1])));
         System.out.println(projectileBlender.toString());
 
         blenderListener.onBlend();
     }
 
-    public void showBlender(){
+    public void showBlender() {
         blenderListener.onShow();
     }
 
-    public void setBlenderListener(BlenderListener blenderListener){
+    public void setBlenderListener(BlenderListener blenderListener) {
         this.blenderListener = blenderListener;
     }
 
-    public interface BlenderListener{
+
+    public interface BlenderListener {
         /**
          * this method is called after game parameters get checked and proved valid.
          */
         void onBlend();
+
         void onShow();
     }
 }
