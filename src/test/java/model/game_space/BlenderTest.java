8672d9e6f41988e9e0be78c71072c5c9a81d2860
Abdul <manofsteel272@yahoo.com>
2020-12-13 20:36:54 +0300

    Added tests for blend method

--- src/test/java/model/game_space/BlenderTest.java
+++ src/test/java/model/game_space/BlenderTest.java
@@ -13,7 +13,51 @@ class BlenderTest {
     @Test
     void blend() {
 
+        ProjectileContainer container = new ProjectileContainer(null, 5, 5, 5, 5, 0, 0, 0, 0);
+        Blender blender = new Blender(null, container);
 
+        assertArrayEquals(container.getAtomMap() , new int[]{5, 5, 5, 5});
+
+        blender.blend(1,2,1); // Blending 2 ALPHA into 1 BETA
+        assertArrayEquals(container.getAtomMap() , new int[]{3, 6, 5, 5});
+
+        blender.blend(1,2,2); // Cannot blend anymore: no enough ALPHA atoms.
+        assertArrayEquals(container.getAtomMap() , new int[]{3, 6, 5, 5});
+
+        blender.blend(4,1,2); // Breaking 2 SIGMA into ALPHA
+        assertArrayEquals(container.getAtomMap() , new int[]{11, 6, 5, 3});
+
+        blender.blend(2,4,2); // Blending 6 BETA into 2 SIGMA
+        assertArrayEquals(container.getAtomMap() , new int[]{11, 0, 5, 5});
+
+
+        container = new ProjectileContainer(null, 3, 3, 3, 3, 0, 0, 0, 0);
+        blender = new Blender(null, container);
+
+        assertArrayEquals(container.getAtomMap() , new int[]{3, 3, 3, 3});
+
+        blender.blend(2,4,1); // Blending 3 BETA into 1 SIGMA
+        assertArrayEquals(container.getAtomMap() , new int[]{3, 0, 3, 4});
+
+        blender.blend(3,2,3); // Blending 3 GAMMA into 6 BETA
+        assertArrayEquals(container.getAtomMap() , new int[]{3, 6, 0, 4});
+
+        container = new ProjectileContainer(null, 0, 0, 0, 0, 0, 0, 0, 0);
+        blender = new Blender(null, container);
+
+        assertArrayEquals(container.getAtomMap() , new int[]{0, 0, 0, 0});
+
+        blender.blend(1,4,1); // Blending 4 ALPHA into 1 SIGMA
+        assertArrayEquals(container.getAtomMap() , new int[]{0, 0, 0, 0});
+
+        blender.blend(2,4,1); // Blending 3 BETA into 1 SIGMA
+        assertArrayEquals(container.getAtomMap() , new int[]{0, 0, 0, 0});
+
+        blender.blend(3,4,1); // Blending 2 GAMMA into 1 SIGMA
+        assertArrayEquals(container.getAtomMap() , new int[]{0, 0, 0, 0});
+
+        blender.blend(4,1,1); // Blending SIGMA into 4 ALPHA
+        assertArrayEquals(container.getAtomMap() , new int[]{0, 0, 0, 0});
 
     }
 }
