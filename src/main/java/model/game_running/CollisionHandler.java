c4c2a05c13672e647ca703edbe378ba57c2fe160
Mouafak alsaid hasan <mhasan18@ku.edu.tr>
2020-12-13 20:28:13 +0300

    documented collisionHandler

--- src/main/java/model/game_running/CollisionHandler.java
+++ src/main/java/model/game_running/CollisionHandler.java
@@ -10,6 +10,11 @@ public class CollisionHandler implements CollisionVisitor {
         this.controller = controller;
     }
 
+    /**
+     * this method calls the removeEntity method of the runningMode object to remove the entities from the game view
+     * @param entity1
+     * @param entity2
+     */
     private void defaultCollision(AutonomousEntity entity1, AutonomousEntity entity2) {
         controller.removeEntity(entity1);
         controller.removeEntity(entity2);
@@ -19,23 +24,47 @@ public class CollisionHandler implements CollisionVisitor {
     // it will be changed a little when we connect this class to the statistics information.
     private int incrementScore(int score){ return score++;}
 
-
+    /**
+     * this method handles the collision of atom entity with a molecule entity. it removes them from the game view.
+     * also, it increment the score of the player by 1
+     * @param atom
+     * @param  molecule
+     */
     @Override
     public void handleCollision(Atom atom, Molecule molecule) {
         defaultCollision(atom, molecule);
         incrementScore(score);
     }
 
+    /**
+     * this method handles the collision of an atom entity with a molecule entity.
+     * it calls the defaultCollision method that call removeEntity method of the RunningMode object to remove both entities from the game view.
+     * @param atom
+     * @param  blocker
+     */
     @Override
     public void handleCollision(Atom atom, Blocker blocker) {
         defaultCollision(atom, blocker);
     }
 
+    /**
+     * this method handles the collision of powerup entity with a molecule entity.
+     * it calls the defaultCollision method that call removeEntity method of the RunningMode object to remove both entities from the game view.
+     * @param powerup
+     * @param  blocker
+     */
     @Override
     public void handleCollision(Powerup powerup, Blocker blocker) {
-        controller.removeEntity(blocker);
+        defaultCollision(powerup, blocker);
     }
 
+    /**
+     * this method handles the collision of powerup entity with a shooter entity.
+     * it calls the defaultCollision method that call removeEntity method of the RunningMode object to remove the powerup from the game view.
+     * and the RunningMode object's method addEntity called to add the powerup object into the entity container.
+     * @param powerup
+     * @param  shooter
+     */
     @Override
     public void handleCollision(Powerup powerup, Shooter shooter) {
         controller.addEntity(powerup);
@@ -43,12 +72,22 @@ public class CollisionHandler implements CollisionVisitor {
     }
 
 
+    /**
+     * this method handles the collision of molecule entity with a blocker entity.
+     * it calls the defaultCollision method that call removeEntity method of the RunningMode object to remove both entities from the game view.
+     * @param molecule
+     * @param  blocker
+     */
     @Override
     public void handleCollision(Molecule molecule, Blocker blocker) {
         defaultCollision(molecule, blocker);
     }
 
-
+    /**
+     * this method handles the collision of blocker entity with a shooter entity.
+     * @param blocker
+     * @param  shooter
+     */
     @Override
     public void handleCollision(Blocker blocker, Shooter shooter) {
         // decrease the health of the player.
