d5bbb2913ac8f064b87298aa1efbe80b8caab55d
Moayed Haji Ali <mali18@ku.edu.tr>
2020-12-23 02:58:05 +0300

    refactor shooter

--- src/main/java/model/game_physics/path_patterns/PathPatternFactory.java
+++ src/main/java/model/game_physics/path_patterns/PathPatternFactory.java
@@ -1,7 +1,8 @@
 package model.game_physics.path_patterns;
-
+import utils.Vector;
 import utils.Velocity;
 
+// TODO: get the entity speed from the configuration
 public class PathPatternFactory {
     private static PathPatternFactory instance = null;
 
@@ -17,14 +18,19 @@ public class PathPatternFactory {
     public PathPattern getBlockerPathPattern(){
         return new StraightPattern(new Velocity(0, 10));
     }
-    public PathPattern getAtomPathPattern(){
-        return new StraightPattern(new Velocity(0, -10));
-    }
     public PathPattern getMoleculePathPattern(){
         return new ZigzagPatten(new Velocity(3, 3), 100);
     }
     public PathPattern getPowerUpPathPattern(){
         return new ZigzagPatten(new Velocity(2, 2), 90);
+    }
+    public PathPattern getAtomPathPattern(double angle){
+        // rotate the vector speed
+        Vector rotatedVector = (new Vector(0, -10)).rotateVector(angle);
+        return new StraightPattern(new Velocity(rotatedVector));
+    }
+    public PathPattern getAtomPathPattern(){
+        return getAtomPathPattern(0);
     }
 
 
