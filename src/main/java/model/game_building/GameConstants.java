2174943863555ea79aa5e4aa6991b3a33772cde6
Abdul <manofsteel272@yahoo.com>
2020-12-23 20:28:43 +0300

    Modified some constants in GameConstants class

--- src/main/java/model/game_building/GameConstants.java
+++ src/main/java/model/game_building/GameConstants.java
@@ -30,7 +30,7 @@ public final class GameConstants {
     public static final int MEDIUM_MODE_GAME_DROP_RATE = 500;
     public static final int HARD_MODE_GAME_DROP_RATE = 250;
 
-    public static final int GAME_THREAD_DELAY = 15;
+    public static final int GAME_THREAD_DELAY = 20; // TODO: Check if valid
 
     // Objects Dimensions.
     public static final Dimension BUILDING_WINDOW_SIZE = new Dimension(800, 800);
