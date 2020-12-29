77bd6b34d3a63591c69a67e4897e471bda20ccb6
Sarieh <ssrdar18@ku.edu.tr>
2020-12-29 05:17:52 +0300

    Reduce image sizes

--- src/main/java/ui/movable_drawables/ShooterDrawer.java
+++ src/main/java/ui/movable_drawables/ShooterDrawer.java
@@ -5,8 +5,10 @@ import model.game_entities.Shooter;
 import utils.Coordinates;
 import utils.MathUtils;
 
+import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.AffineTransform;
+import java.net.URL;
 
 /**
  * This class is responsible for drawing the Shooter given the Shooter entity in the constructor
@@ -15,11 +17,12 @@ public class ShooterDrawer implements Drawable {
 
     private final Shooter shooter;
     private final Image shooterImage;
+    private Image shooterImageGif;
 
     public ShooterDrawer(Shooter shooter) {
         this.shooter = shooter;
         this.shooterImage = ImageResources.get(shooter);
-
+        this.shooterImageGif = ImageResources.getGif();
     }
 
     @Override
@@ -27,17 +30,21 @@ public class ShooterDrawer implements Drawable {
         Graphics2D g2d = (Graphics2D) g;
         AffineTransform old = g2d.getTransform();
 
-        g2d.rotate(Math.toRadians(shooter.getAngle()), (int) shooter.getCoordinates().getX(), (int) shooter.getCoordinates().getY());
+        g2d.rotate(Math.toRadians(shooter.getAngle()),
+                (int) shooter.getCoordinates().getX(),
+                (int) shooter.getCoordinates().getY());
+
         Projectile projectile = shooter.getCurrentProjectile();
         Coordinates drawingCoordinates = MathUtils.drawingCoordinates(shooter.getCoordinates(),
                 shooter.getHitbox().getWidth(),
                 shooter.getHitbox().getHeight());
 
-        g2d.drawImage(shooterImage, drawingCoordinates.getPoint().x, drawingCoordinates.getPoint().y, null);
+        g2d.drawImage(shooterImageGif, drawingCoordinates.getPoint().x, drawingCoordinates.getPoint().y, null);
         if (projectile != null) {
             Coordinates projectileCoord = MathUtils.drawingCoordinates(
                     shooter.getCoordinates(),
                     0, projectile.getHitbox().getHeight() + shooter.getHitbox().getHeight());
+
             projectile.setCoordinates(projectileCoord);
             DrawableFactory.get(projectile).draw(g);
         }
