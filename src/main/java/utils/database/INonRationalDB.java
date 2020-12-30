8dc8a78145ad4ec0b62f723fc9670406aec2b8fb
Moayed Haji Ali <mali18@ku.edu.tr>
2020-12-31 02:42:21 +0300

    add registerCollection, removeCollection, and update to interface

--- src/main/java/utils/database/INonRationalDB.java
+++ src/main/java/utils/database/INonRationalDB.java
@@ -1,10 +1,16 @@
 package utils.database;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import java.io.IOException;
 import java.util.List;
 
 public interface INonRationalDB {
-//    boolean deleteCollection(String collectionTitle)
+    boolean deleteCollection(String collectionTitle);
     boolean registerCollection(String collectionTitle);
-    <T> boolean save(String collectionTitle, Class<T> classType, T instance);
-    <T> List<T> get(String collectionTitle, Class<T> classType);
+    <T> boolean save(String collectionTitle, String uniqueID, T instance) throws IOException;
+    <T> boolean update(String collectionTitle, String uniqueID, T instance) throws IOException;
+    boolean delete(String collectionTitle, String uniqueID);
+    <T> T load(String collectionTitle, String uniqueID, Class<T> tClass) throws IOException;
+
 }
